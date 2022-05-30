cfa0b80235c2aa5efaeb5bb4e57989daddbf1fc1
andgarzonmal <andgarzonmal@unal.edu.co>
2022-05-30 12:00:19 -0500

    create html module

---
+++ src/modules/innerhtml.js
@@ -0,0 +1,16 @@
+const tasksHtml = ({index, description}) => `
+<li class="draggable list" id="${index}" draggable="true">
+    <input type="checkbox" name="task" >
+    <input type="text"  class="input-list" value="${description}">
+    <div>
+        <i class="material-icons dots">
+            more_vert
+        </i>
+        <i class="material-icons delete">
+            delete
+        </i>
+    </div>
+</li>
+`
+
+export default tasksHtml
