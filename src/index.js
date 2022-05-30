e83b2ed34bb511f8bed3494dcf58364cb43aec76
andgarzonmal <andgarzonmal@unal.edu.co>
2022-05-30 11:57:05 -0500

    add array of objects and add functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-import './style.css'
+import './style.css';
+
+const form = document.querySelector(".form");
+const inputForm = document.querySelector(".input-form");
+const submitBtn = document.querySelector(".submit");
+const allTasks = document.querySelector(".tasks");
+
+let todoTasks = []; 
+
+const tasksHtml = ({index, description}) => `
+<li class="draggable list" id="${index}" draggable="true">
+    <input type="checkbox" name="task" >
+    <input type="text"  class="input-list" value="${description}">
+    <div>
+        <i class="material-icons dots">
+            more_vert
+        </i>
+        <i class="material-icons delete">
+            delete
+        </i>
+    </div>
+</li>
+`
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  addTask()
+  createTasks()
+})
+
+const addTask = e => {
+  if(inputForm.value.trim() === '') {
+    return
+  }
+
+  const task = {
+    index: Date.now(),
+    description: inputForm.value,
+    completed: false
+  }
+
+  todoTasks.push(task);
+  inputForm.value='';
+  inputForm.focus();
+}
+
+const createTasks = () => {
+  allTasks.innerHTML ='';
+  todoTasks.forEach(task => {
+    allTasks.insertAdjacentHTML("beforeend",tasksHtml(task))
+  })  
+}
