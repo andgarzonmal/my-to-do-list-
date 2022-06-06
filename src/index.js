602628e99847ebd83e4a58f2cd0a911a46a511eb
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-06 13:57:00 -0500

    delete coments

--- src/index.js
+++ src/index.js
@@ -16,37 +16,6 @@ const allTasks = document.querySelector('.tasks');
 const savedLists = JSON.parse(localStorage.getItem('todoTasks'));
 let todoTasks = getFromLocalStorage();
 
-// const del = (e) => {
-//   if (e.target.classList.contains('delete')) {
-//     let arrOfTasks = getFromLocalStorage()
-//     const deletebtns = Array.from(document.querySelectorAll('.delete'));
-//     console.log(deletebtns.indexOf(e.target) + 1)
-//     arrOfTasks = arrOfTasks.filter((x) => x.index !== deletebtns.indexOf(e.target) + 1);
-//     e.target.parentNode.remove();
-//     for (let i = 0; i < arrOfTasks.length; i += 1) {
-//       arrOfTasks[i].index = i + 1;
-//     }
-//     localStorage.setItem('todoTasks', JSON.stringify(arrOfTasks));
-//   }
-// };
-
-// const clearAll = (e) => {
-//   if (e.target.classList.contains('clear')) {
-//     let arrOfTasks = getFromLocalStorage()
-//     const allToDelete = document.querySelectorAll('.list');
-//     allToDelete.forEach((toDelete) => {
-//       if (toDelete.childNodes[1].checked) {
-//         toDelete.remove();
-//         arrOfTasks = arrOfTasks.filter((x) => x.completed !== true);
-//       }
-//       for (let i = 0; i < arrOfTasks.length; i += 1) {
-//         arrOfTasks[i].index = i + 1;
-//       }
-//       localStorage.setItem('todoTasks', JSON.stringify(arrOfTasks));
-//     });
-//   }
-// };
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addTask();
