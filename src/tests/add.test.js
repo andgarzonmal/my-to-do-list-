9ac26535da6a01d246c59ce4f64be8db5e6bbe54
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-09 14:18:02 -0500

    add tests of adding functionalities

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,103 +1,63 @@
+import JSDOMEnvironment, { TestEnvironment } from 'jest-environment-jsdom';
 import {
     tasksHtml, addTask, createTasks, getFromLocalStorage,
   } from '../modules/add.js';
 
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+describe('Add function', ()=>{
 
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
+  beforeEach(()=>{
+    document.body.innerHTML = `
+    <div>
+      <form class="form" class="dflex">
+          <input type="text" value="task1" class="input-form" autofocus>
+          <button class="material-icons submit" type="submit">
+            keyboard_return
+          </button>
+      </form>
+      <ul class="tasks">
+      </ul>
+    </div>`
 
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = String(value);
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-global.localStorage = new LocalStorageMock;
-
-const mydom = new JSDOM (`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<title>My to Do</title>
-</head>
-<body class="dflex">
-
-	<h1 id="my-todo-title">✔️ My todo list ✔️</h1>
-	<main class="dflex">
-		<header class="header dflex">
-			<h3 class="h-title">Today's To Do</h3>
-			<span class="material-symbols-outlined autorenew">
-				autorenew
-			</span>
-		</header>
-		
-		<form class="form" class="dflex">
-			<input type="text" placeholder="Add to your list..." class="input-form" autofocus>
-			<button class="material-icons submit" type="submit">
-				keyboard_return
-			</button>
-		</form>
-
-		<ul class="tasks"> </ul>
-		
-		<button type="button" class="clear">
-			clear all completed
-		</button>
-	</main>
-</body>
-</html>`)
-
-
-describe('tasksHtml', () => {
-  test('returns a string', () => {
-    expect(typeof tasksHtml(2,"desc")).toBe('string')
-  })
-})
-
-describe('addTask', ()=> {
-  const inputForm = mydom.window.document.querySelector('.input-form');
-  inputForm.value = ""
+    localStorage.clear();
+    jest.clearAllMocks();
+    localStorage.setItem.mockClear();
   
-  test('function is returned if value is empty',()=> {
-    expect(addTask(inputForm)).toBe(undefined)
-  })
-
-  inputForm.value = "task1"
-  
-  addTask(inputForm)
-
-  test('new task has been added', () => {
-    expect(JSON.parse(localStorage.store.todoTasks).length).toBe(1)
-  })
-
-  test('new task is task1', () => {
-    expect(JSON.parse(localStorage.store.todoTasks)[0].description).toBe("task1")
+    
   })
   
-  const allTasks = mydom.window.document.querySelector('.tasks');
-  createTasks(allTasks)
-  const list = [...mydom.window.document.querySelectorAll('.list')]
-  
-  test('new task added to the DOM',()=> {
-    expect(list.length).toBe(1)
+  test('Adds a task to local storage', ()=>{
+    const inputForm = document.querySelector('.input-form')
+
+    expect(JSON.parse(localStorage.getItem('todoTasks'))).toBeNull()
+    addTask(inputForm);
+
+    expect(JSON.parse(localStorage.getItem('todoTasks')).length).toBe(1)  
+    inputForm.value = "task2"
+    addTask(inputForm);
+
+    expect(JSON.parse(localStorage.getItem('todoTasks')).length).toBe(2)  
+  })
+
+  test('If the input is empty, don\'t add to localStorage', ()=>{
+    const inputForm = document.querySelector('.input-form')
+    inputForm.value = '';
+
+    expect(JSON.parse(localStorage.getItem('todoTasks'))).toBeNull()
+    addTask(inputForm);
+    expect(JSON.parse(localStorage.getItem('todoTasks'))).toBeNull()
+
+  })
+
+  test('adds task to DOM', () => {
+    const inputForm = document.querySelector('.input-form')
+    const allTasks = document.querySelector('.tasks')
+    addTask(inputForm);
+    createTasks(allTasks);
+    expect(allTasks.children.length).toBe(1)
+
+    inputForm.value = "task2"
+    addTask(inputForm);
+    createTasks(allTasks);
+    expect(allTasks.children.length).toBe(2)
   })
 })
